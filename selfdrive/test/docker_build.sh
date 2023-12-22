ed8e988ba6c7944b1125bf02573f7933b283dc62
aurnik <aurnikislam@gmail.com>
2023-12-22 14:28:11 -0800

    (temp) Comment out docker build step

--- selfdrive/test/docker_build.sh
+++ selfdrive/test/docker_build.sh
@@ -17,10 +17,10 @@ fi
 
 source $SCRIPT_DIR/docker_common.sh $1 "$TAG_SUFFIX"
 
-DOCKER_BUILDKIT=1 docker buildx build --pull --platform $PLATFORM --load --cache-to type=inline --cache-from type=registry,ref=$REMOTE_TAG -t $REMOTE_TAG -t $LOCAL_TAG -f $OPENPILOT_DIR/$DOCKER_FILE $OPENPILOT_DIR
+# DOCKER_BUILDKIT=1 docker buildx build --pull --platform $PLATFORM --load --cache-to type=inline --cache-from type=registry,ref=$REMOTE_TAG -t $REMOTE_TAG -t $LOCAL_TAG -f $OPENPILOT_DIR/$DOCKER_FILE $OPENPILOT_DIR
 
-if [ -n "$PUSH_IMAGE" ]; then
-  docker push $REMOTE_TAG
-  docker tag $REMOTE_TAG $REMOTE_SHA_TAG
-  docker push $REMOTE_SHA_TAG
-fi
+# if [ -n "$PUSH_IMAGE" ]; then
+#   docker push $REMOTE_TAG
+#   docker tag $REMOTE_TAG $REMOTE_SHA_TAG
+#   docker push $REMOTE_SHA_TAG
+# fi
